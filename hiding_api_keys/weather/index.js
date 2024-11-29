1b29590e00412c0fa564d450de982bff51b64ed3
naraabinnadhikari <narayanabin223@gmail.com>
2024-11-29 20:24:07 +0545

    working urls and apis

--- hiding_api_keys/weather/index.js
+++ hiding_api_keys/weather/index.js
@@ -1,9 +1,32 @@
 import express from "express"
 import fetch from "node-fetch"
-
 const router = express.Router();
+
+const fetchWeather = async (searchtext) => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchtext}&units=metric&APPID=${process.env.WEATHER_API_KEY}`;
+    
+    try {
+        const weatherStream = await fetch(url);
+        const weatherJson = await weatherStream.json();
+        return weatherJson;
+    } catch(err) {
+        return { Error: err.stack};
+    }
+}
 
 router.get("/", (req,res) => res.json({success: "Weather is good.", failure: "Weather is bad."
 }));
+
+router.get("/:searchtext", async (req,res) => {
+    const searchtext = req.params.searchtext;
+    const data = await fetchWeather(searchtext);
+    res.json(data);
+})
+
+router.post("/", async (req,res) => {
+    const searchtext = req.body.searchtext;
+    const data = await fetchWeather(searchtext);
+    res.json(data);
+})
 
  export default router 
